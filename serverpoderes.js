b9de368a627468cf7d5ac7f4b1ffd81459204105
SRodriguez0801 <131110417+SRodriguez0801@users.noreply.github.com>
2023-07-28 21:18:33 -0600

    tercero

--- serverpoderes.js
+++ serverpoderes.js
@@ -13,7 +13,7 @@ heroes.post('/api/Heroes',(req, res)=>{
       nombre : req.body.nombre,
       identidad_secreta : req.body.identidad_secreta,
       activo : req.body.activo,
-      fecha_horas : req.body.fecha_horas
+      fecha_borra : req.body.fecha_borra
 
     };
 
@@ -22,5 +22,59 @@ heroes.post('/api/Heroes',(req, res)=>{
     res.json(tmpheroes);
 
 });
+
+heroes.post('/api/Poderes',(req, res)=>{
+
+
+  let tmpPoderes ={
+    id : req.body.id,
+    nombre : req.body.nombre,
+    activo : req.body.activo,
+    fecha_borra : req.body.fecha_borra
+
+  };
+
+  arreglo.push(tmpPoderes);
+  console.log(JSON.stringify(arreglo));
+  res.json(tmpPoderes);
+
+});
+
+heroes.post('/api/HeroesPoder',(req, res)=>{
+
+
+  let tmpheroes_poder ={
+    id : req.body.id,
+    id_poder : req.body.id_poder,
+    id_heroe : req.body.id_heroe,
+    activo : req.body.activo,
+    fecha_borra : req.body.fecha_borra
+
+  };
+
+  arreglo.push(tmpheroes_poder);
+  console.log(JSON.stringify(arreglo));
+  res.json(tmpheroes_poder);
+
+});
+
+heroes.get('/api/Heroes',(req, res)=>{
+ 
+  res.json(arreglo);
+
+});
+
+heroes.get('/api/Poderes',(req, res)=>{
+ 
+  res.json(arreglo);
+
+});
+
+heroes.get('/api/HeroesPoder',(req, res)=>{
+ 
+  res.json(arreglo);
+
+});
+
 heroes.listen(4000);
 
